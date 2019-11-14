9d72b6e5dac0e07be468122c0a909341cbf942bc
Andreas Schneider <asn@samba.org>
2019-11-14 10:35:27 +0100

    pkgconfig: Fix path to library in nss_wrapper.pc
    Signed-off-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Samuel Cabrero <scabrero@samba.org

--- nss_wrapper.pc.cmake
+++ nss_wrapper.pc.cmake
@@ -1,4 +1,4 @@
 Name: @PROJECT_NAME@
 Description: The nss_wrapper library
 Version: @PROJECT_VERSION@
-Libs: @CMAKE_INSTALL_LIBDIR@/@NSS_WRAPPER_LIB@
+Libs: @CMAKE_INSTALL_FULL_LIBDIR@/@NSS_WRAPPER_LIB@
