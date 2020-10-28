5d2b47fdd89c3834b226a56703cb51be8b937d21
TJ Miller <millert@us.ibm.com>
2020-10-28 13:54:48 -0700

    nwrap_files_cache_reload: add test for closed handles error
    Reviewed-by: Andreas Schneider <asn@samba.org>
    Reviewed-by: Ralph Boehme <slow@samba.org>

--- tests/test_getpwuid_module.c
+++ tests/test_getpwuid_module.c
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include <setjmp.h>
 #include <cmocka.h>
+#include <unistd.h>
 
 #include <pwd.h>
 
@@ -22,12 +23,36 @@ static void test_nwrap_passwd(void **state)
 	assert_int_equal(pwd->pw_gid, id);
 }
 
+static void test_nwrap_passwd_closed_handles(void **state)
+{
+	struct passwd *pwd;
+	uid_t id = 424242;
+	long maxfd;
+
+	(void) state; /* unused */
+
+	pwd = getpwuid(id);
+	assert_non_null(pwd);
+
+	maxfd = sysconf(_SC_OPEN_MAX);
+	if (maxfd < 0) {
+		maxfd = 1024;
+	}
+	for (long fd = 3; fd < maxfd; fd++) {
+		close(fd);
+	}
+
+	pwd = getpwuid(id);
+	assert_non_null(pwd);
+}
+
 int main(void)
 {
 	int rc;
 
 	const struct CMUnitTest tests[] = {
 		cmocka_unit_test(test_nwrap_passwd),
+		cmocka_unit_test(test_nwrap_passwd_closed_handles),
 	};
 
 	rc = cmocka_run_group_tests(tests, NULL, NULL);
